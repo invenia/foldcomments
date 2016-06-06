a76bfdb5b3128207e2a439bec2c48e64ca22dde8
Curtis Vogt <curtis.vogt@gmail.com>
2016-06-06 10:13:38 -0500

    Add a behavioural test

---
+++ user-tests/test.py
@@ -0,0 +1,83 @@
+# pylint: disable-all
+# flake8: noqa
+
+# Settings for matching expected results:
+# {
+#    "concatenate_adjacent_comments": false,
+#    "fold_single_line_comments": false,
+#    "show_first_line": true,
+#    "autofold": true | false,
+#    "fold_strings": true,
+# }
+
+# For each test the section before the "pass" keyword is what is too be
+# folded. What comes after "pass" is a representation of how the folded
+# text is expected to look like.
+
+
+def comment_test1():
+    # Comments that are not inline and occur right next to
+    # each other should be combined into one.
+
+    # Standalone lines should not be combined
+
+    pass
+    # Comments that are not inline and occur right next to ...
+
+    # Standalone lines should not be combined
+
+
+def comment_test2():
+    """
+    A docstring
+    """
+    # Comment adjacent to docstring
+
+    pass
+    """ ... A docstring ... """
+    # Comment adjacent to docstring
+
+
+def comment_test3():
+    var = []  # inline comment
+
+    # Comment referring loop below which shouldn't be combined
+    for i in range(10):
+        var.append(i)
+
+    pass
+    var = []  # inline comment
+
+    # Comment referring loop below which shouldn't be combined
+    for i in range(10):
+        var.append(i)
+
+
+def string_test1():
+    var = (
+        "only triple quoted strings "
+        "should be folded"
+    )
+    pass
+    var = (
+        "only triple quoted strings "
+        "should be folded"
+    )
+
+
+def string_test2():
+    # """
+    # Commented out docstring should be treated as
+    # a multiline comment
+    # """
+    pass
+    # """ ...
+
+
+def string_test3():
+    var = """
+        triple quoted string used in an assignment
+        shouldn't be auto folded
+        """
+    pass
+    var = """ ... triple quoted string used in an assignment ... """
